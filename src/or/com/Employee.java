ef0e700e1223e32b5690e5ac0741b38cdd25fad8
kasiessawrNaga <kasi14061998@gmail.com>
2022-02-07 10:37:07 +0530

    message

---
+++ src/or/com/Employee.java
@@ -0,0 +1,14 @@
+package or.com;
+
+public class Employee {
+	public void add() {
+System.out.println("Print");
+	}
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		e.add();
+		
+		
+	}
+
+}
